b299f373205e9fe7bc270012403e842eef6eb017
HiromiHiguruma <156872734+HiromiHiguruma@users.noreply.github.com>
2024-01-26 18:27:58 -0300

    Add files via upload

---
+++ Ejercicio7_intermedio_itertools.py
@@ -0,0 +1,93 @@
+#itertools son herramientas para manejar listas, tuplas,etc.
+
+#product
+from itertools import product
+
+#El producto sirve para combinar en contenedores separados los elementos de un contenedor combinados con otro contenedor
+
+Lista_1 = [1,2]
+
+Lista_2 = [3,4]
+
+Producto = product(Lista_1, Lista_2)
+
+print (list(Producto))
+
+Lista_3 = [1,2]
+
+Lista_4 = [3]
+
+#--------------------------------------------------
+
+Producto2 = product(Lista_3, Lista_4, repeat = 2)
+
+print (list(Producto2))
+
+
+#--------------------------------------------------
+
+from itertools import permutations
+
+#permutation devuelve todas las combinaciones posbiles
+
+Lista_5 = [8,9,10]
+
+permutacion = permutations (Lista_5)
+
+print (list(permutacion))
+
+#se puede aclarar la cantidad de elementos en la lista 
+
+permutacion2 = permutations (Lista_5, 2)
+
+print (list(permutacion2))
+#---------------------------------------------------------
+
+from itertools import combinations
+
+#Las combinaciones muestran combinaciones con una cantidad especificada (es obligatorio especificar) de elementos. No se combinan los mismos elementos.
+
+
+Lista_6 = [11,12,13,14]
+
+combinacion = combinations (Lista_6, 2)
+
+print (list(combinacion))
+
+#las combinaciones con remplazo son lo mismo solo que los elementos si se combinan con ellos mismos.
+from itertools import combinations_with_replacement
+
+Lista_7 = [15,16,17,18]
+
+combinacion_con_reemplazo = combinations_with_replacement(Lista_7,2)
+
+print (list(combinacion_con_reemplazo))
+#------------------------------------------------------------
+from itertools import accumulate
+#acumulate sirve para realizar operaciones sobre los elementos de las lista.Se empieza del índice 0 y 1, y va prosiguiendo aplicando el resultado de la operación 
+#con el número siguiente
+#Por defecto realiza suma
+
+Lista_8 = [10,20,30]
+
+acumulacion = accumulate (Lista_8)
+print (list(acumulacion))
+
+#si se quiere realizar otra operación que no sea suma
+import operator
+
+acumulacion_multiplicada = accumulate (Lista_8 , func=operator.mul)
+
+print (list(acumulacion_multiplicada))
+
+#---------------------------------------------------------
+#groupby organiza los elementos dentro de un contenedor mediante un criterio generalmente expresado con una función
+from itertools import groupby
+Lista_10 = [1,2,3,4,5,6,7,8,9]
+
+def mayor_a_5 (x):
+    return x > 5
+
+grupo_por = groupby (Lista_10, key= mayor_a_5)
+for key, value in grupo_por:
+    print (key, list(value))
